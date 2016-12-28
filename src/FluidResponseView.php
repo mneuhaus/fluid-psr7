060acb22c295b23c277d93e55636ee23d2990a94
Marc Neuhaus <marc@mia3.com>
2016-12-28 19:58:36 +0100

    importe templateView

--- src/FluidResponseView.php
+++ src/FluidResponseView.php
@@ -2,24 +2,7 @@
 
 namespace MIA3\FluidPsr7;
 
+use TYPO3Fluid\Fluid\View\TemplateView;
+
 class FluidResponseView extends TemplateView {
-    public function __construct()
-    {
-//        $FLUID_CACHE_DIRECTORY = !isset($FLUID_CACHE_DIRECTORY) ? __DIR__ . '/../cache/' : $FLUID_CACHE_DIRECTORY;
-//
-//        $paths = $view->getTemplatePaths();
-//        $paths->setTemplateRootPaths([
-//            __DIR__ . '/../Resources/Private/Templates/'
-//        ]);
-//        $paths->setLayoutRootPaths([
-//            __DIR__ . '/../Resources/Private/Layouts/'
-//        ]);
-//        $paths->setPartialRootPaths([
-//            __DIR__ . '/../Resources/Private/Partials/'
-//        ]);
-//        if ($FLUID_CACHE_DIRECTORY) {
-//            // Configure View's caching to use ./examples/cache/ as caching directory.
-//            $view->setCache(new \TYPO3Fluid\Fluid\Core\Cache\SimpleFileCache($FLUID_CACHE_DIRECTORY));
-//        }
-    }
 }
